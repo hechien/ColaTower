34211b917a55068f205e4d001223853055f1a0a6
HeChien Hsu <hechien@me.com>
2012-11-29 10:46:05 +0800

    First initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ColaTower::Application.config.secret_token = 'e37e20b2abf0a25b7b14e99021696d24a66705e02324cd0c5906faee6959741abe9ce893486e4846894eb97f1ef5efd61f7ca3f9751ca0d1650ede080589afa3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ColaTower::Application.config.session_store :cookie_store, key: '_ColaTower_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ColaTower::Application.config.session_store :active_record_store
